fef7f50b0dc41d6fdaf17fc801905cf965e213d9
YuhApps <yuhapps@gmail.com>
2022-02-16 05:25:36 +0700

    Updated to Electron 16, switched from Electron-Settings to Settings script, fixed Dark scrollbar.

---
+++ src/settings.js
@@ -0,0 +1,33 @@
+const app = require('electron').app || require('@electron/remote').app
+const fs = require('fs')
+const path = require('path')
+
+const CONFIG_JSON_PATH = app.getPath('userData') + path.sep + 'settings.json'
+
+let settings
+
+module.exports.get = function(key) {
+    if (settings) {
+        return settings[key]
+    } else if (fs.existsSync(CONFIG_JSON_PATH)) {
+        settings = JSON.parse(fs.readFileSync(CONFIG_JSON_PATH, 'utf-8')) || {}
+        return settings[key]
+    } else {
+        return undefined
+    }
+}
+
+module.exports.refresh = function() {
+    settings = JSON.parse(fs.readFileSync(CONFIG_JSON_PATH, 'utf-8')) || {}
+}
+
+module.exports.set = function(key, value) {
+    settings[key] = value
+    if (fs.existsSync(CONFIG_JSON_PATH)) {
+        let json = JSON.parse(fs.readFileSync(CONFIG_JSON_PATH, 'utf-8')) || {}
+        json[key] = value
+        fs.writeFileSync(CONFIG_JSON_PATH, JSON.stringify(json))
+    } else {
+        fs.writeFileSync(CONFIG_JSON_PATH, JSON.stringify({ key: value }))
+    }
+}
